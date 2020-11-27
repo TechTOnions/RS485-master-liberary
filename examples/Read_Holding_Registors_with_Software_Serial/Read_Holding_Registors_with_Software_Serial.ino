3b868fa4bd4aedbd5ed24c9a489e663aa856fd5c
Dharmik Bhavani <tdharmik768@gmail.com>
2020-11-27 11:11:22 +0530

    added library

---
+++ examples/Read_Holding_Registors_with_Software_Serial/Read_Holding_Registors_with_Software_Serial.ino
@@ -0,0 +1,78 @@
+/*
+   This library can be used with both Software serial & hardware serial.
+   This example shows you how to Read Holding registors(FC=03) from slave device with slave ID 1 and baud 9600, Data bit 8, parity none, stop bit 1
+
+   This example uses Software serial port
+*/
+
+/***********************************************************************
+  Written by Dharmik.
+  Copyright (C) 2020
+  gpl-3.0, all text above must be included in any redistribution
+ ***********************************************************************/
+ 
+#include <RS485.h>
+#include<SoftwareSerial.h>
+
+SoftwareSerial RS485_slave1_port(10,11); //rx tx pin define here 
+
+//use below function like this if not using an enable pin for RS485, else comment it
+RS485 TO_RS485 (&RS485_slave1_port);  //calling defined port to RS485 lib
+
+//if using enable pin for RS485 uncoment below code and comment above line
+/*
+  #define RS485_enable_pin 1      // define your digital enable pin over here
+  RS485 TO_RS485 (&RS485_slave1_port,RS485_enable_pin);
+*/
+
+float received_value;  // variable for storing Received value over RS485
+unsigned int address1 = 100;  // address you want to read for this example we will read Holding Registe address 100
+float gain = 1;   // gain value will be multiplied with the orignal value recived from slave and after multiplying it will return
+void setup() {
+  Serial.begin(9600);
+  TO_RS485.begin(9600);                 //define baud here for your RS485
+  // Note: Make sure that slave device is working on same baud rate.
+  //       right now library only supports data bit 8, parity none & stop bit 1
+
+}
+
+void loop() {
+
+  //read_holding_reg(slave_ID,address,Data_type,Data_order,gain);
+
+  // here 1 is slave ID
+  // address1 = 100
+
+  //Data_types formate which slave will give response we can use following
+  // floats
+  // uint32
+  // int32
+  // uint16
+  // int16
+
+  //Data_order order in which slave will send bytes
+  //let say slave gives 32-bit data then there will be four byte data let say A B C D  & if 16-bit data will be two byte A B
+  // So Data orders can be classified as
+  // big_endian             (32-bit AB CD)(16-bit AB) High byte first   &   High word first
+  // mid_big_endian         (32-bit BA CD)            Low  byte  firt   &   High word first
+  // mid_little_endian      (32-bit CD AB)            High byte first   &   Low  word first
+  // little_endian          (32-bit DC BA)(16-bit BA) Low  byte first   &   Low  word first
+
+  // gain value will be multiplied with the orignal value recived from slave and after multiplying it will return
+
+  received_value = TO_RS485.read_holding_reg(1, address1, floats, big_endian, gain); // uses Function Code 03
+  if (received_value == -99.99)
+  {
+    Serial.println("TIME OUT ERROR.....");
+  }
+  else if (received_value == -88.88)
+  {
+    Serial.println("CRC FAIL.....");
+  }
+  else
+  {
+    Serial.print("Value: ");
+    Serial.println(received_value);
+  }
+  delay(1000);
+}
